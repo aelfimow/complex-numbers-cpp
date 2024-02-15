4cbdd602931db0d12eb96ac2930a3fe6987053c2
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-15 19:05:35 +0100

    Beispiel_5_1: number with __float128

--- std_complex/Beispiel_5_1.cpp
+++ std_complex/Beispiel_5_1.cpp
@@ -1,7 +1,43 @@
 #include <complex>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <quadmath.h>
 
 using namespace std::complex_literals;
+
+namespace
+{
+    /**
+     * \brief Helper to convert __float128 to std::string
+     */
+    std::string to_string(__float128 const &value)
+    {
+        std::vector<char> buffer(256U);
+
+        ::quadmath_snprintf(buffer.data(), buffer.size(), "%.3Qe", value);
+
+        std::string const str(buffer.data());
+
+        return str;
+    }
+
+    /**
+     * \brief Helper to convert std::complex<__float128> to std::string
+     */
+    std::string to_string(std::complex<__float128> const &value)
+    {
+        std::string str { };
+
+        str.append("(");
+        str.append(to_string(value.real()));
+        str.append(",");
+        str.append(to_string(value.imag()));
+        str.append(")");
+
+        return str;
+    }
+}
 
 /**
  * \brief Beispiel 5.1 (p. 297)
@@ -29,5 +65,12 @@ void Beispiel_5_1()
 
         std::cout << z << std::endl;
         std::cout << "Re(z) = " << z.real() << ", Im(z) = " << z.imag() << std::endl;
+    }
+    // Using: __float128
+    {
+        std::complex<__float128> z(3.0Q, 7.0Q);
+
+        std::cout << to_string(z) << std::endl;
+        std::cout << "Re(z) = " << to_string(z.real()) << ", Im(z) = " << to_string(z.imag()) << std::endl;
     }
 }
