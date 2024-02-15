f65865e681ed8e0ab8f3ce9b328e7eda09371c87
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-15 18:52:07 +0100

    Beispiel_5_1: use double

--- std_complex/Beispiel_5_1.cpp
+++ std_complex/Beispiel_5_1.cpp
@@ -5,8 +5,16 @@ using namespace std::complex_literals;
 
 void Beispiel_5_1()
 {
+    // Using: float
     {
         std::complex<float> z = 3.0f + 7.0if;
+
+        std::cout << z << std::endl;
+        std::cout << "Re(z) = " << z.real() << ", Im(z) = " << z.imag() << std::endl;
+    }
+    // Using: double
+    {
+        std::complex<double> z = 3.0 + 7.0i;
 
         std::cout << z << std::endl;
         std::cout << "Re(z) = " << z.real() << ", Im(z) = " << z.imag() << std::endl;
