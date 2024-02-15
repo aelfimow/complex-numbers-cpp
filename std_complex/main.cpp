5fe27246963cfde63c26c991e0f91e19d345a3fe
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-15 20:08:19 +0100

    main: added Beispiel 5.2

--- std_complex/main.cpp
+++ std_complex/main.cpp
@@ -1,9 +1,13 @@
-#include <cstdlib>
 #include "Beispiel_5_1.h"
+#include "Beispiel_5_2.h"
+
+#include <cstdlib>
+
 
 int main(int, char *[])
 {
     Beispiel_5_1();
+    Beispiel_5_2();
 
     return EXIT_SUCCESS;
 }
