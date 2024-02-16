d05a397a20b2681e887f2f28b198f2ec374ef727
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-16 20:37:46 +0100

    Helper: added argument

--- std_complex/Helper.cpp
+++ std_complex/Helper.cpp
@@ -6,11 +6,11 @@
 /**
  * \brief Helper to convert __float128 to std::string
  */
-std::string helper::to_string(__float128 const &value)
+std::string helper::to_string(__float128 const &value, bool precise)
 {
     std::vector<char> buffer(256U);
 
-    ::quadmath_snprintf(buffer.data(), buffer.size(), "%.3Qe", value);
+    ::quadmath_snprintf(buffer.data(), buffer.size(), precise ? "%.36Qe" : "%.3Qe", value);
 
     std::string const str(buffer.data());
 
@@ -20,14 +20,14 @@ std::string helper::to_string(__float128 const &value)
 /**
  * \brief Helper to convert std::complex<__float128> to std::string
  */
-std::string helper::to_string(std::complex<__float128> const &value)
+std::string helper::to_string(std::complex<__float128> const &value, bool precise)
 {
     std::string str { };
 
     str.append("(");
-    str.append(to_string(value.real()));
+    str.append(to_string(value.real(), precise));
     str.append(", ");
-    str.append(to_string(value.imag()));
+    str.append(to_string(value.imag(), precise));
     str.append(")");
 
     return str;
