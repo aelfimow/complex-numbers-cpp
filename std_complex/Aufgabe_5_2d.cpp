8c208a47beb160460dbc2e14217e14e98700ee52
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-18 10:50:35 +0100

    Aufgabe_5_2d: added files

---
+++ std_complex/Aufgabe_5_2d.cpp
@@ -0,0 +1,34 @@
+#include "Aufgabe_5_2d.h"
+#include "Helper.h"
+
+#include <complex>
+#include <iostream>
+#include <cmath>
+#include <quadmath.h>
+
+using namespace std::complex_literals;
+
+namespace
+{
+    /**
+     * \brief Aufgabe 5.2d (p. 300) using float
+     * \see Book1.md
+     */
+    void Aufgabe_5_2d(float a)
+    {
+        std::complex<float> const z(a, a);
+
+        float const rho = std::abs(z);
+
+        std::cout << z << ": " << rho << std::endl;
+    }
+}
+
+/**
+ * \brief Aufgabe 5.2d (p. 300)
+ * \see Book1.md
+ */
+void Aufgabe_5_2d()
+{
+    Aufgabe_5_2d(1.0f);
+}
