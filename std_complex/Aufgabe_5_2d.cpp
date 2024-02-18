8512c016ab2277c5afe3b5e944e02f2e611691f1
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-18 10:55:48 +0100

    Aufgabe_5_2d: using __float128

--- std_complex/Aufgabe_5_2d.cpp
+++ std_complex/Aufgabe_5_2d.cpp
@@ -48,6 +48,30 @@ namespace
 
         std::cout << z << ": " << rho << std::endl;
     }
+
+    /**
+     * \brief Aufgabe 5.2d (p. 300) using __float128
+     * \see Book1.md
+     */
+    void Aufgabe_5_2d(__float128 a)
+    {
+        std::complex<__float128> const z(a, a);
+
+#if 0
+        __float128 const rho = std::abs(z); // does not compile
+#else
+        __float128 const rho1 = ::sqrtq((z.real() * z.real()) + (z.imag() * z.imag()));
+        __float128 const rho2 = ::sqrtq(::powq(z.real(), 2.0Q) + ::powq(z.imag(), 2.0Q));
+        __float128 const diff = (rho2 - rho1);
+#endif
+
+        bool const precise = true;
+
+        std::cout << helper::to_string(z, precise) << ":" << std::endl;
+        std::cout << "    rho1 = " << helper::to_string(rho1, precise) << std::endl;
+        std::cout << "    rho2 = " << helper::to_string(rho2, precise) << std::endl;
+        std::cout << "    diff = " << helper::to_string(diff, precise) << std::endl;
+    }
 }
 
 /**
@@ -59,4 +83,5 @@ void Aufgabe_5_2d()
     Aufgabe_5_2d(1.0f);
     Aufgabe_5_2d(1.0);
     Aufgabe_5_2d(1.0L);
+    Aufgabe_5_2d(1.0Q);
 }
