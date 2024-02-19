0bedd68bc90ce3ff96427efa91710146e0302d7e
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-19 18:09:37 +0100

    Beispiel_5_1: using double

--- _Complex/Beispiel_5_1.cpp
+++ _Complex/Beispiel_5_1.cpp
@@ -1,7 +1,6 @@
 #include "Beispiel_5_1.h"
 #include "Helper.h"
 
-#include <complex.h>
 #include <iostream>
 
 using namespace std::complex_literals;
@@ -19,6 +18,16 @@ void Beispiel_5_1()
     // Using: float
     {
         _Complex float z = 3.0f + 7.0fi;
+
+        std::cout << helper::to_string(z) << std::endl;
+        std::cout << "Re(z) = " << __real__ z << ", Im(z) = " << __imag__ z << std::endl;
+    }
+    // Using: double
+    {
+#if 0
+        _Complex double z = 3.0 + 7.0i; // Compiler complains about std::complex<double>
+#endif
+        _Complex double z = 3.0 + 7.0j;
 
         std::cout << helper::to_string(z) << std::endl;
         std::cout << "Re(z) = " << __real__ z << ", Im(z) = " << __imag__ z << std::endl;
