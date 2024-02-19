139f0eaec3f03b584d87b2ae92a3f2c156404640
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-19 17:50:56 +0100

    Beispiel_5_1: using float

--- _Complex/Beispiel_5_1.cpp
+++ _Complex/Beispiel_5_1.cpp
@@ -1,6 +1,7 @@
 #include "Beispiel_5_1.h"
 #include "Helper.h"
 
+#include <complex.h>
 #include <iostream>
 
 using namespace std::complex_literals;
@@ -17,9 +18,9 @@ void Beispiel_5_1()
     // z = 3 + 7i
     // Using: float
     {
-        _Complex float z = 3.0f + 7.0if;
+        _Complex float z = 3.0f + 7.0fi;
 
-        std::cout << z << std::endl;
-        std::cout << "Re(z) = " << z.real() << ", Im(z) = " << z.imag() << std::endl;
+        std::cout << helper::to_string(z) << std::endl;
+        std::cout << "Re(z) = " << __real__ z << ", Im(z) = " << __imag__ z << std::endl;
     }
 }
