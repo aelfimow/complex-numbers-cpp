f494fa922193db43e779f66bc553b2ce0bc727f3
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-19 18:18:32 +0100

    Beispiel_5_1: using __complex128

--- _Complex/Beispiel_5_1.cpp
+++ _Complex/Beispiel_5_1.cpp
@@ -1,6 +1,7 @@
 #include "Beispiel_5_1.h"
 #include "Helper.h"
 
+#include <quadmath.h>
 #include <iostream>
 
 using namespace std::complex_literals;
@@ -38,5 +39,12 @@ void Beispiel_5_1()
 
         std::cout << helper::to_string(z) << std::endl;
         std::cout << "Re(z) = " << __real__ z << ", Im(z) = " << __imag__ z << std::endl;
+    }
+    // Using: __float128
+    {
+        __complex128 const z = 3.0Q + 7.0Qi;
+
+        std::cout << helper::to_string(z) << std::endl;
+        std::cout << "Re(z) = " << helper::to_string(__real__ z) << ", Im(z) = " << helper::to_string(__imag__ z) << std::endl;
     }
 }
