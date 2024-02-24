8126b76cdb7babbc92cf67f457fb0d161b54e194
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-24 21:40:41 +0100

    Beispiel_5_2: using long double

--- _Complex/Beispiel_5_2.cpp
+++ _Complex/Beispiel_5_2.cpp
@@ -171,4 +171,11 @@ void Beispiel_5_2()
 
         std::cout << helper::to_string(z) << " -> " << helper::to_string(cz) << std::endl;
     }
+    // Using: long double
+    {
+        _Complex long double const z = 5.0L;
+        _Complex long double const cz = conjugate(z);
+
+        std::cout << helper::to_string(z) << " -> " << helper::to_string(cz) << std::endl;
+    }
 }
