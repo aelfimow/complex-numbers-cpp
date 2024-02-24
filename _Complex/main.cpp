f173d15ebb9d8148e0ec9a8ab42a9d3e296bbad3
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-24 21:20:52 +0100

    main: use new function

--- _Complex/main.cpp
+++ _Complex/main.cpp
@@ -1,4 +1,5 @@
 #include "Beispiel_5_1.h"
+#include "Beispiel_5_2.h"
 
 #include <cstdlib>
 #include <iostream>
@@ -8,6 +9,9 @@ int main(int, char *[])
 {
     std::cout << "Beispiel_5_1: " << std::endl;
     Beispiel_5_1();
+
+    std::cout << "Beispiel_5_2: " << std::endl;
+    Beispiel_5_2();
 
     return EXIT_SUCCESS;
 }
