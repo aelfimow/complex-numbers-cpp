f32c8bf3d57112a18e86941722201ff8c1a57e01
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-24 21:39:55 +0100

    Beispiel_5_2: using double

--- _Complex/Beispiel_5_2.cpp
+++ _Complex/Beispiel_5_2.cpp
@@ -164,4 +164,11 @@ void Beispiel_5_2()
 
         std::cout << helper::to_string(z) << " -> " << helper::to_string(cz) << std::endl;
     }
+    // Using: double
+    {
+        _Complex double const z = 5.0;
+        _Complex double const cz = conjugate(z);
+
+        std::cout << helper::to_string(z) << " -> " << helper::to_string(cz) << std::endl;
+    }
 }
