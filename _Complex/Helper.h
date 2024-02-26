5f8603cf6483b1594ad0a188f682b891cdaeec18
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-26 18:57:37 +0100

    Helper: added functions

--- _Complex/Helper.h
+++ _Complex/Helper.h
@@ -17,6 +17,11 @@ namespace helper
     extern std::string to_string(_Complex double const &value, bool precise = false);
     extern std::string to_string(_Complex long double const &value, bool precise = false);
     extern std::string to_string(__complex128 const &value, bool precise = false);
+
+    float abs(_Complex float const &z);
+    double abs(_Complex double const &z);
+    long double abs(_Complex long double const &z);
+    __float128 abs(__complex128 const &z);
 }
 
 #endif
