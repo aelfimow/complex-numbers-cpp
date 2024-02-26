da2d0794514f4882b072f989d7d97799eeb54585
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-26 18:58:26 +0100

    Aufgabe_5_2b: added files

---
+++ _Complex/Aufgabe_5_2b.cpp
@@ -0,0 +1,24 @@
+#include "Aufgabe_5_2b.h"
+#include "Helper.h"
+
+#include <cmath>
+#include <iostream>
+#include <quadmath.h>
+
+using namespace std::complex_literals;
+
+/**
+ * \brief Aufgabe 5.2b (p. 300)
+ * \see Book1.md
+ */
+void Aufgabe_5_2b()
+{
+    // Using float
+    {
+        _Complex float const z = 2.0f - 3.0fi;
+
+        float const rho = helper::abs(z);
+
+        std::cout << helper::to_string(z) << ": " << rho << std::endl;
+    }
+}
---
+++ _Complex/Aufgabe_5_2b.h
@@ -0,0 +1,8 @@
+#ifdef AUFGABE_5_2B_H
+#error Already included.
+#else
+#define AUFGABE_5_2B_H
+
+extern void Aufgabe_5_2b();
+
+#endif
