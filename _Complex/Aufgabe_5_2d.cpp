0244275c182b00d74acbcaec0b3ad7deef3bc4bc
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-28 19:02:02 +0100

    Aufgabe_5_2d: using __complex128

--- _Complex/Aufgabe_5_2d.cpp
+++ _Complex/Aufgabe_5_2d.cpp
@@ -47,6 +47,19 @@ namespace
 
         std::cout << helper::to_string(z) << ": " << rho << std::endl;
     }
+
+    /**
+     * \brief Aufgabe 5.2d using __complex128
+     * \see Book1.md, (Seite 300)
+     */
+    void Aufgabe_5_2d(__float128 a)
+    {
+        __complex128 const z = a + (a * 1.0Qi);
+
+        __float128 const rho = helper::abs(z);
+
+        std::cout << helper::to_string(z) << ": " << helper::to_string(rho) << std::endl;
+    }
 }
 
 /**
@@ -58,4 +71,5 @@ void Aufgabe_5_2d()
     Aufgabe_5_2d(1.0f);
     Aufgabe_5_2d(1.0);
     Aufgabe_5_2d(1.0L);
+    Aufgabe_5_2d(1.0Q);
 }
