156874a7092b2f2715d3f98e8ff2c6ceeeac58b8
Alexander Elfimow <my.free.mail@gmx.de>
2024-02-28 18:48:31 +0100

    Aufgabe_5_2c: added files

---
+++ _Complex/Aufgabe_5_2c.cpp
@@ -0,0 +1,24 @@
+#include "Aufgabe_5_2c.h"
+#include "Helper.h"
+
+#include <cmath>
+#include <iostream>
+#include <quadmath.h>
+
+using namespace std::complex_literals;
+
+/**
+ * \brief Aufgabe 5.2b
+ * \see Book1.md, (Seite 300)
+ */
+void Aufgabe_5_2c()
+{
+    // Using float
+    {
+        _Complex float const z = ::sqrtf(2.0f) + (::sqrtf(7.0f) * 1.0fi);
+
+        float const rho = helper::abs(z);
+
+        std::cout << helper::to_string(z) << ": " << rho << std::endl;
+    }
+}
