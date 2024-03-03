a35b0d1f43fe1f66c53789fd16ffd1ce6e13be28
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-03 18:25:13 +0100

    Beispiel_5_8: moved to function

--- std_complex/Beispiel_5_8.cpp
+++ std_complex/Beispiel_5_8.cpp
@@ -9,14 +9,14 @@
 using namespace std::complex_literals;
 
 /**
- * \brief Beispiel 5.8
+ * \brief Beispiel 5.8 with z1
  * \see Book1.md, (Seite 304)
  */
-void Beispiel_5_8()
+static void Beispiel_5_8_1()
 {
     // Using: float
     {
-        std::complex<float> z = (2.0f * ::sqrtf(3.0f)) + 2.0if;
+        std::complex<float> const z = (2.0f * ::sqrtf(3.0f)) + 2.0if;
 
         float const phi = std::arg(z);
         float const degree = helper::to_degree(phi);
@@ -27,7 +27,7 @@ void Beispiel_5_8()
     }
     // Using: double
     {
-        std::complex<double> z = (2.0 * ::sqrt(3.0)) + 2.0i;
+        std::complex<double> const z = (2.0 * ::sqrt(3.0)) + 2.0i;
 
         double const phi = std::arg(z);
         double const degree = helper::to_degree(phi);
@@ -38,7 +38,7 @@ void Beispiel_5_8()
     }
     // Using: long double
     {
-        std::complex<long double> z = (2.0L * ::sqrtl(3.0L)) + 2.0Li;
+        std::complex<long double> const z = (2.0L * ::sqrtl(3.0L)) + 2.0Li;
 
         long double const phi = std::arg(z);
         long double const degree = helper::to_degree(phi);
@@ -49,7 +49,7 @@ void Beispiel_5_8()
     }
     // Using: __float128
     {
-        std::complex<__float128> z((2.0Q * ::sqrtq(3.0)), 2.0Q);
+        std::complex<__float128> const z((2.0Q * ::sqrtq(3.0)), 2.0Q);
 
         __float128 const phi = helper::arg(z);
         __float128 const degree = helper::to_degree(phi);
@@ -58,4 +58,13 @@ void Beispiel_5_8()
             << ", phi (rad) = " << helper::to_string(phi)
             << ", phi (degree) = " << helper::to_string(degree) << std::endl;
     }
+}
+
+/**
+ * \brief Beispiel 5.8
+ * \see Book1.md, (Seite 304)
+ */
+void Beispiel_5_8()
+{
+    Beispiel_5_8_1();
 }
