c4f3c8733c7ac8125c151255e4b04d5c22d79e5d
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-03 12:13:01 +0100

    Helper: added function

--- std_complex/Helper.h
+++ std_complex/Helper.h
@@ -11,6 +11,8 @@ namespace helper
     extern std::string to_string(__float128 const &value, bool precise = false);
 
     extern std::string to_string(std::complex<__float128> const &value, bool precise = false);
+
+    float to_degree(float value);
 }
 
 #endif
