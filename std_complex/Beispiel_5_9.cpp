ed7995ccfd025b861e4d089cb216130fbc886eb1
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-06 20:11:17 +0100

    Beispiel_5_9: using double

--- std_complex/Beispiel_5_9.cpp
+++ std_complex/Beispiel_5_9.cpp
@@ -25,4 +25,15 @@ void Beispiel_5_9()
 
         std::cout << "using float: " << z << "; imag^2: " << i2 << std::endl;
     }
+
+    // Using: double
+    {
+        double const alpha = ((5.0 * std::numbers::pi_v<double>) / 3.0);
+
+        std::complex<double> const z = (2.0 * (::cos(alpha) + (1.0i * ::sin(alpha))));
+
+        double const i2 = (z.imag() * z.imag());
+
+        std::cout << "using double: " << z << "; imag^2: " << i2 << std::endl;
+    }
 }
